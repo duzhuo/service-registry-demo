90f643c10744c80a522ef296e52af5b799935917
duzhuo <duzhuoloveyou@163.com>
2016-02-26 10:57:51 +0800

    Initial commit

---
+++ src/main/java/com/zjs/ServiceRegistryDemoApplication.java
@@ -0,0 +1,14 @@
+package com.zjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryDemoApplication.class, args);
+    }
+}
